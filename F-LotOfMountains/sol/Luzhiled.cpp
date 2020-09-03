cac9702396585375ec87a43be0eff19dbf8f8be9
Luzhiled <Luzhiled+github@gmail.com>
2020-09-03 16:49:40 +0900

    add solver

---
+++ F-LotOfMountains/sol/Luzhiled.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    map< int, double > mp;
+
+    for (int i = 0; i < n; ++i) {
+        int x, h, r;
+        cin >> x >> h >> r;
+        
+        double t = (double)h / r;
+        mp[x - r] += t;
+        mp[x] -= 2 * t;
+        mp[x + r] += t;
+    }
+
+    double ans = 0;
+    double h = 0, dh = 0;
+
+    auto iterl = mp.begin(), iterr = mp.begin();
+    ++iterr;
+
+    while (iterr != mp.end()) {
+        dh += iterl->second;
+        h += (iterr->first - iterl->first) * dh;
+
+        ans = max(ans, h);
+
+        ++iterr;
+        ++iterl;
+    }
+
+    cout << fixed << setprecision(10) << ans << endl;
+}
