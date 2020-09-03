3d7303774d769b120e421645b13752ddb2ab7e6c
keymoon <kymn0116@gmail.com>
2020-09-03 14:58:12 +0900

    fix checker

--- C_BrokenMachine/checker.cpp
+++ C_BrokenMachine/checker.cpp
@@ -8,9 +8,8 @@
 
 #include <iostream>
 
-using ll = long long;
+using ll = int64_t;
 using namespace std;
-using P = pair<int, int>;
 
 int main(int argc, char *argv[]) {
     registerTestlibCmd(argc, argv);
@@ -18,20 +17,18 @@ int main(int argc, char *argv[]) {
     int n = inf.readInt();
 
     int m = ouf.readInt(M_MIN, M_MAX);
-
+    
     vector<bool> has_in(n, false);
     vector<vector<int>> edges(n, vector<int>{});
     for (int i = 0; i < m; i++){
         int v = ouf.readInt(1, n) - 1;
         int w = ouf.readInt(1, n) - 1;
-        cout << v << " " << w << endl;
         if (v >= w) quitf(_wa, "edge should v < w");
         edges[v].push_back(w);
         has_in[w] = true;
     }
-
-    vector<int> order(n);
-    iota(order.begin(), order.end(), 0);
+    if (!ouf.seekEof())
+        quitf(_wa, "Participant output contains extra tokens");
 
     vector<ll> init_values(n);
     for (int i = 0; i < n; i++){
@@ -41,10 +38,10 @@ int main(int argc, char *argv[]) {
     set<int> cands{};
     for (int broken = 0; broken < n; broken++){
         vector<ll> values = init_values;
-        for (auto&& node : order){
+        for (int node = 0; node < n; node++){
             ll nextValue = broken == node ? values[node] * 2 : values[node];
             for (auto adj : edges[node]){
-                values[adj] = (values[adj] + nextValue) % (1L << 32);
+                values[adj] = (values[adj] + nextValue) % (ll(1) << 32);
             }
         }
         cands.insert(values[n - 1]);
