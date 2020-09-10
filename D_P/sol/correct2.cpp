2b3cd57b4736b6a304a3647fef39d38fcbefc013
ok126 <ok1260026@gmail.com>
2020-09-10 15:19:03 +0900

    fix p

--- D_P/sol/correct2.cpp
+++ D_P/sol/correct2.cpp
@@ -7,50 +7,22 @@ using namespace std;
 bool check(vector<long long> &b, vector<long long> &c, long long N, long long P, long long num) {
 	long long tsum = 0, tsum2 = 0;
 	vector<long long> d(N);
-	
-	c.clear();
-	c.resize(N, 0);
+
+	c.resize(N);
 	
 	for(int i = 0; i < N; i++){ 
-		
-		{
-			long long l = -1, h = num + 10, m;
-			
-			while(l + 1 < h){
-				m = (l + h) / 2;
-				if(m * P / num < b[i]) {
-					l = m;
-				} else {
-					h = m;
-				}
-			}
-			
-			c[i] = h;
-		}
+		c[i] = (b[i] * num + P - 1) / P;
 		
 		if(c[i] * P / num != b[i]) return false;
 		tsum += c[i];
-		
-		{
-			long long l = 0, h = num + 10, m;
-			
-			while(l + 1 < h){
-				m = (l + h) / 2;
-				
-				if(m * P / num < b[i] + 1) {
-					l = m;
-				} else {
-					h = m;
-				}
-			}
-			
-			d[i] = l;
-		}
+		d[i] = ((b[i] + 1)* num + P - 1) / P;
+		d[i]--;
 		
 		assert(c[i] * P / num == d[i] * P / num);
 	}
 	
 	if(tsum > num) return false;
+	
 	
 	for(int i = N-1; i >= 0; i--){
 		if(tsum == num) break;
@@ -114,6 +86,7 @@ signed main(){
 	
 	for(int i = 0; i < N; i++){
 		cin>>B[i];
+		
 		sum += B[i];
 	}
 	
