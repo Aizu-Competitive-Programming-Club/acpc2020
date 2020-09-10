048e6e56009ed99dfdd23d6c365ea382c2489d11
beet <20266375+beet-aizu@users.noreply.github.com>
2020-09-10 14:58:49 +0900

    change template

--- htmlgen.py
+++ htmlgen.py
@@ -50,23 +50,18 @@ def to_keyword_str(lang_dict):
 keywords = {
     'statement': to_keyword_str({
         'en': 'Problem Statement',
-        'ja': '問題文',
     }),
     'constraints': to_keyword_str({
         'en': 'Constraints',
-        'ja': '制約',
     }),
     'input': to_keyword_str({
         'en': 'Input',
-        'ja': '入力',
     }),
     'output': to_keyword_str({
         'en': 'Output',
-        'ja': '出力',
     }),
     'sample': to_keyword_str({
         'en': 'Sample',
-        'ja': 'サンプル',
     }),
 }
 
@@ -98,13 +93,13 @@ def gen_params(toml_params):
 class ExampleReader:
     sample_template = '''
 <div class="uk-grid-small uk-child-width-1-2@s" uk-grid>
-    <div><pre>{}</pre></div>
-    <div><pre>{}</pre></div>
+    <h2>Sample Input {}</h2><pre>{}</pre>
+    <h2>Sample Output {}</h2><pre>{}</pre>
 </div>
 '''
 
     def __init__(self, problem_dir: Path):
-        self.counter = 1
+        self.counter = 0
         self.problem_dir = problem_dir
         self.used = set()  # type: Set[str]
 
@@ -119,10 +114,9 @@ class ExampleReader:
         infile = open(str(inpath), 'r').read()
         outfile = open(str(outpath), 'r').read()
 
-        s = r'<h3># {}</h3>'.format(self.counter)
         self.counter += 1
-        s += self.sample_template.format(infile, outfile)
-        return s
+        return self.sample_template.format(self.counter, infile,
+                                           self.counter, outfile)
 
     def check_all_used(self) -> bool:
         for file_path in Path(self.problem_dir).glob('in/example_*.in'):
@@ -151,126 +145,10 @@ class LangManager:
         self.now_lang = ''
         return s
 
-
-class ForumExpander(Preprocessor):
-    forum_template = '''
-<a class="uk-button uk-button-default" href={url}>Forum <span uk-icon="icon: link"></span></a>
-'''
-
-    def __init__(self, url):
-        self.url = url[0]
-
-    def run(self, lines):
-        new_lines = []
-        for line in lines:
-            new_lines.append(line)
-        if self.url:
-            new_lines.append('')
-            new_lines.append('---')
-            new_lines.append('')
-            new_lines.append(self.forum_template.format(url=self.url))
-            new_lines.append('')
-        return new_lines
-
-
-class ForumExtension(Extension):
-    def __init__(self, **kwargs):
-        self.config = {
-            'url': ['dummy issue URL', 'URL'],
-        }
-        super(ForumExtension, self).__init__(**kwargs)
-
-    def extendMarkdown(self, md):
-        md.preprocessors.register(ForumExpander(
-            self.config['url']), 'forum', 101)
-
-
 html_header = '''
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <!-- Uikit -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://judge.yosupo.jp/public/css/uikit.min.css" />
-    <script src="https://judge.yosupo.jp/public/js/uikit.min.js"></script>
-    <script src="https://judge.yosupo.jp/public/js/uikit-icons.min.js"></script>
-    <!-- Katex -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.css"
-        integrity="sha384-yFRtMMDnQtDRO8rLpMIKrtPCD5jdktao2TV19YiZYWMDkUR5GQZR/NOVTdquEx1j" crossorigin="anonymous">
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/katex.min.js"
-        integrity="sha384-9Nhn55MVVN0/4OFx7EE5kpFBPsEMZxKTCnA+4fqDmg12eCTqGi6+BB2LjY8brQxJ"
-        crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.10.2/dist/contrib/auto-render.min.js"
-        integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"
-        onload="renderMathInElement(document.body);"></script>
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            renderMathInElement(
-                document.body, {
-                    delimiters: [
-                        { left: "$$", right: "$$", display: true },
-                        { left: "$", right: "$", display: false }],
-                    ignoredTags: [],
-                })
-        });
-    </script>
-    <script>
-        function getLang(lang) {
-            lang = localStorage.getItem("lang");
-            if (lang == "ja" || lang == "en") return lang;
-            for (e of navigator.languages) {
-                if (e.startsWith("en")) return "en";
-                if (e.startsWith("ja")) return "ja";
-            }
-            return "en";
-        }
-        function refLang() {
-            lang = getLang();
-            document.getElementById("nav-lang").text = `Lang(${lang})`;
-
-
-            document.querySelectorAll(".lang-en, .lang-ja").forEach(function(e) {
-                if (e.classList.contains(`lang-${lang}`)) {
-                    e.style.display = 'block';
-                } else {
-                    e.style.display = 'none';
-                }
-            })
-        }
-        function setLang(lang) {
-            localStorage.setItem("lang", lang)
-            refLang()
-        }
-        document.addEventListener("DOMContentLoaded", refLang);
-    </script>
-</head>
 '''
 html_body = '''
-<body>
-    <div class="uk-navbar-container" uk-navbar>
-        <div class="uk-navbar-right">
-            <ul class="uk-navbar-nav">
-                <li>
-                    <a href="#" id="nav-lang">Lang()</a>
-                    <div class="uk-navbar-dropdown">
-                        <ul class="uk-nav uk-navbar-dropdown-nav">
-                            <li><a href="#" onclick="setLang('ja')">Ja</a></li>
-                            <li><a href="#" onclick="setLang('en')">En</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
-
-    <section class="uk-section">
-        <div class="uk-container">
-            {}
-        </div>
-    </section>
-</body>
-</html>
+{}
 '''
 
 
@@ -278,7 +156,8 @@ class ToHTMLConverter:
     def __init__(self, probdir: Path, config):
         logger.info("HTML Generate {}".format(probdir.name))
         md_statement = open(str(probdir / 'task.md'), encoding='utf-8').read()
-
+        md_statement = md_statement.replace('```\n', '<pre>', 1)
+        md_statement = md_statement.replace('\n```', '</pre>', 1)
         # evaluate jinja2
         lang_manager = LangManager()
         environment = Environment(
@@ -298,7 +177,6 @@ class ToHTMLConverter:
             mid_statement, extensions=[
                 'md_in_html',
                 'markdown.extensions.fenced_code',
-                ForumExtension(url=config.get('forum', ''))
             ],
         )
         self.html = html_header + html_body.format(self.statement)
