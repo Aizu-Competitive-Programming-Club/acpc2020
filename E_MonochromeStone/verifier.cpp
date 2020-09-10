39f62b349aa160df0e8bb714575efe15b155ec24
mo3tthi(plan) <sdvxkaede@gmail.com>
2020-09-10 18:07:30 +0900

    fix N_P_K_(MIN,MAX)

--- E_MonochromeStone/verifier.cpp
+++ E_MonochromeStone/verifier.cpp
@@ -3,16 +3,16 @@
 
 int main() {
     registerValidation();
-    int N = inf.readInt(N_W_B_MIN, N_W_B_MAX); // N
+    int N = inf.readInt(N_P_K_MIN, N_P_K_MAX); // N
     inf.readChar(' ');
-    inf.readInt(N_W_B_MIN, N_W_B_MAX); // W
+    inf.readInt(N_P_K_MIN, N_P_K_MAX); // P
     inf.readChar(' ');
-    inf.readInt(N_W_B_MIN, N_W_B_MAX); // B
+    inf.readInt(N_P_K_MIN, N_P_K_MAX); // K
     inf.readChar('\n');
     for (int i = 0; i < N; i++) {
-      inf.readInt(N_W_B_MIN, N_W_B_MAX); // WI
+      inf.readInt(N_P_K_MIN, N_P_K_MAX); // w_i
       inf.readChar(' ');
-      inf.readInt(N_W_B_MIN, N_W_B_MAX); // BI
+      inf.readInt(N_P_K_MIN, N_P_K_MAX); // b_i
       inf.readChar('\n');
      
     }
