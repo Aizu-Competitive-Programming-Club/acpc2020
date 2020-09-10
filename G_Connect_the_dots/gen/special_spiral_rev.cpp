2c9317f0e37068148148ab5df948d43d8e78dc3a
moritaoy <moritaoy@outlook.jp>
2020-09-10 17:15:14 +0900

    add testcase

--- G_Connect_the_dots/gen/special_spiral_rev.cpp
+++ G_Connect_the_dots/gen/special_spiral_rev.cpp
@@ -17,7 +17,7 @@ int main(int, char* argv[]) {
 
     vector<int> x_sel,y_sel;
     set<int> xl,yl;
-    int k = N/8;
+    int k = N/4;
     lp(i,k){
       int x,y;
       while(1){
@@ -36,23 +36,23 @@ int main(int, char* argv[]) {
     sort(x_sel.begin(),x_sel.end());
     sort(y_sel.begin(),y_sel.end());
     vector<pair<int,int>> v,w;
-    lp(i,k){
+    lp(i,k/2){
       v.push_back({x_sel[i], y_sel[i]});
-      v.push_back({x_sel[N/8-1-i], y_sel[N/8-1-i]});
-      w.push_back({x_sel[i],y_sel[N/8-1-i]});
-      w.push_back({x_sel[N/8-1-i],y_sel[i]});
-    }
-    lp(j,w.size()){
-      int i = w.size()-1-j;
-      int l = gen.uniform<int>(1,3);
-      cout<<w[i].first-l<<" "<<w[i].second-l<<endl;
-      cout<<w[i].first+l<<" "<<w[i].second+l<<endl;
+      v.push_back({x_sel[k-1-i], y_sel[k-1-i]});
+      w.push_back({x_sel[i],y_sel[k-1-i]});
+      w.push_back({x_sel[k-1-i],y_sel[i+1]});
     }
     lp(j,v.size()){
       int i = v.size()-1-j;
       int l = gen.uniform<int>(1,3);
-      cout<<v[i].first-l<<" "<<v[i].second-l<<endl;
-      cout<<v[i].first+l<<" "<<v[i].second+l<<endl;
+      cout<<v[i].first+l<<" "<<v[i].second<<endl;
+      cout<<v[i].first-l<<" "<<v[i].second<<endl;
+    }
+    lp(j,w.size()){
+      int i = w.size()-1-j;
+      int l = gen.uniform<int>(1,3);
+      cout<<w[i].first<<" "<<w[i].second+l<<endl;
+      cout<<w[i].first<<" "<<w[i].second-l<<endl;
     }
     return 0;
 }
